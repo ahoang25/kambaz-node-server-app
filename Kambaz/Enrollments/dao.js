753cfbed00727938d171caf6c5128150ebd62acc
ahoang25 <122462255+ahoang25@users.noreply.github.com>
2025-04-17 06:03:49 -0400

    a6

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -19,10 +19,13 @@ export async function findCoursesForUser(userId) {
  const enrollments = await model.find({ user: userId }).populate("course");
  return enrollments.map((enrollment) => enrollment.course);
 }
-export async function findUsersForCourse(courseId) {
- const enrollments = await model.find({ course: courseId }).populate("user");
- return enrollments.map((enrollment) => enrollment.user);
-}
+export const findUsersForCourse = async (courseId) => {
+  const enrollments = await enrollmentModel
+    .find({ course: courseId })
+    .populate("user");
+
+  return enrollments;
+};
 export function enrollUserInCourse(user, course) {
   const newEnrollment = { user, course, _id: `${user}-${course}` };
   return model.create(newEnrollment);
