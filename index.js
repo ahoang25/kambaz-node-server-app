e420837705614e5fbc6eac5f824cbdff5029a852
ahoang25 <122462255+ahoang25@users.noreply.github.com>
2025-04-17 02:15:35 -0400

    a6

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import session from "express-session";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.NETLIFY_URL ,
-    })
-   );   
-   const sessionOptions = {
-    secret: process.env.SESSION_SECRET ,
-    resave: false,
-    saveUninitialized: false,
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  })
+ );   
+ const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
   };
-  if (process.env.NODE_ENV === "production") {
-    app.set("trust proxy", 1); 
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN, 
-    };
-  }
+}
   
   app.use(session(sessionOptions));  
   
