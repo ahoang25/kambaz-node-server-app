40a426731b4daf0b8c576ba93b8a1e861638dc64
ahoang25 <122462255+ahoang25@users.noreply.github.com>
2025-04-20 22:56:15 -0400

    finalProject

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:5174",
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
   })
  );   
  const sessionOptions = {
